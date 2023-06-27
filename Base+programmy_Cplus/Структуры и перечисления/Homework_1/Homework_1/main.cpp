fc48427ff4587872ca1cdbda682ea0bb8f1eb18d
Aleksey Shabanov <ashabanov781@gmail.com>
2023-06-27 17:55:49 +0300

    Structures and recount CORRECTION_1 HW_1

--- Base+programmy_Cplus/Структуры и перечисления/Homework_1/Homework_1/main.cpp
+++ Base+programmy_Cplus/Структуры и перечисления/Homework_1/Homework_1/main.cpp
@@ -31,51 +31,55 @@ int main() {
         std::cout << "Введите номер месяца: ";
         std::cin >> x;
         //std::cout << x << std::endl;
+        
+        if ((x > 0) && (x < 13)) {       
+            switch (static_cast<int>(x))
+            {
+            case 1:
+                std::cout << "Январь" << std::endl;
+                break;
+            case 2:
+                std::cout << "Февраль" << std::endl;
+                break;
+            case 3:
+                std::cout << "Март" << std::endl;
+                break;
+            case 4:
+                std::cout << "Апрель" << std::endl;
+                break;
+            case 5:
+                std::cout << "Май" << std::endl;
+                break;
+            case 6:
+                std::cout << "Июнь" << std::endl;
+                break;
+            case 7:
+                std::cout << "Июль" << std::endl;
+                break;
+            case 8:
+                std::cout << "Август" << std::endl;
+                break;
+            case 9:
+                std::cout << "Сентябрь" << std::endl;
+                break;
+            case 10:
+                std::cout << "Октябрь" << std::endl;
+                break;
+            case 11:
+                std::cout << "Ноябрь" << std::endl;
+                break;
+            case 12:
+                std::cout << "Декабрь" << std::endl;
+                break;
 
-       
-        switch (static_cast<int>(x))
-        {
-        case 1:
-            std::cout << "Январь" << std::endl;
-            break;
-        case 2:
-            std::cout << "Февраль" << std::endl;
-            break;
-        case 3:
-            std::cout << "Март" << std::endl;
-            break;
-        case 4:
-            std::cout << "Апрель" << std::endl;
-            break;
-        case 5:
-            std::cout << "Май" << std::endl;
-            break;
-        case 6:
-            std::cout << "Июнь" << std::endl;
-            break;
-        case 7:
-            std::cout << "Июль" << std::endl;
-            break;
-        case 8:
-            std::cout << "Август" << std::endl;
-            break;
-        case 9:
-            std::cout << "Сентябрь" << std::endl;
-            break;
-        case 10:
-            std::cout << "Октябрь" << std::endl;
-            break;
-        case 11:
-            std::cout << "Ноябрь" << std::endl;
-            break;
-        case 12:
-            std::cout << "Декабрь" << std::endl;
-            break;
-
+            }
         }
-        //if ((x < 0) || (x > 13))
+        if ((x < 0) || (x > 12)) {
+            std::cout << "Неверное число, повторите ввод!" << std::endl;
+        }
+       
             
 
-    } while ((x > 0) && (x < 13));
+    } while (x != 0);
     std::cout << "Неправильный номер!" << std::endl;
 }
