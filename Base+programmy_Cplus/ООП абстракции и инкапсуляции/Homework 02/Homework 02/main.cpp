4493b947393f5367cfbf9462371a0fd4727a5149
Aleksey Shabanov <ashabanov781@gmail.com>
2023-07-31 17:37:47 +0300

    OOP: Obstruct and incapsule HW_1_2 FIXES_2

--- Base+programmy_Cplus/ООП абстракции и инкапсуляции/Homework 02/Homework 02/main.cpp
+++ Base+programmy_Cplus/ООП абстракции и инкапсуляции/Homework 02/Homework 02/main.cpp
@@ -12,74 +12,64 @@ private:
     std::string full_adress;
 
 public:
-    Adress(std::string town, std::string street, int building, int appart) {
+    void set_adress(std::string town, std::string street, int building, int appart) {
         this->town = town;
         this->street = street;
         this->building = building;
         this->appart = appart;
-    };
-
-    std::string create_fulladress() {
-        full_adress = town + " , " + street + " , " + std::to_string(building) + " , " + std::to_string(appart);
-        return full_adress;
-    };
-};
-
-//Функция создания текстового динамического массива
-std::string* creat_arr(int count) {
-    std::string* arr = new std::string[count]{};
-    return arr;
-}
-
-
-//Функция сортировки
-void sort(std::string* arr, int count) {
-
-    bool swapped = false;
-    do {
-        swapped = false;
-        std::string tmp;
-
-        for (int i = count - 1; i > 0; --i) {
-            if (arr[i - 1] > arr[i]) {
-                tmp = arr[i];
-                arr[i] = arr[i - 1];
-                arr[i - 1] = tmp;
-                swapped = true;
-            }
-        }
-    } 
-    while (swapped);
-};
-
-
-//Функция печати массива в файл
-void print_arr(std::string* arr, int count) {
-    std::ofstream fileOut("out.txt");
-
-    if (fileOut.is_open()) {
-        std::cout << "Файл out.txt открыт." << std::endl;
-    }
-    else {
-        std::cout << "Ошибка открытия файла" << std::endl;
     }
 
-    fileOut << "Всего количество адресов: " << count << std::endl << std::endl;
 
-    fileOut << "Адреса:" << std::endl;
-
-    for (int i = 0; i < count; ++i) {
-        fileOut << arr[i] << std::endl;
+    std::string get_town() {
+        return town;
     }
-    fileOut.close();
+
+    std::string get_street() {
+        return street;
+    }
+
+    int get_building() {
+        return building;
+    }
+
+    int get_appart() {
+        return appart;
+    }
+
+    // std::string create_fulladress() {
+    //     full_adress = town + " , " + street + " , " + std::to_string(building) + " , " + std::to_string(appart);
+    //     return full_adress;
+    // }
+
+    int converting(std::string x) {
+        int x2 = 0;
+        x2 = std::stoi(x);
+        return x2;
+    }
+
+
+    void set_convert(Adress& A, Adress& B) {
+        std::string c1, c2;
+        int c3, c4;
+        c1 = B.town;
+        c2 = B.street;
+        c3 = B.building;
+        c4 = B.appart;
+
+        B.town = A.town;
+        B.street = A.street;
+        B.building = A.building;
+        B.appart = A.appart;
+
+
+        A.town = c1;
+        A.street = c2;
+        A.building = c3;
+        A.appart = c4;
+
+    }
+
 };
-
-//Функция удаления динамического массива
-void delete_arr(std::string* arr, int count) {
-    delete[] arr;
-    arr = nullptr;
-}
-
 
 
 
@@ -88,37 +78,65 @@ int main() {
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
 
-    std::ifstream file("in.txt"); // окрываем файл для чтения
-    std::string infile;
-
+    std::ifstream file("in.txt"); 
     if (!file) {
-        std::cout << "Ошибка открытия файла in.txt." << std::endl;
+        std::cout << "Fail open file in.txt." << std::endl;
     }
     else {
-        std::cout << "Файл in.txt успешно открыт." << std::endl;
+        std::cout << "File in.txt is opened" << std::endl;
     }
 
     std::string town;
     std::string street;
-    int building;
-    int appart;
+    std::string building;
+    std::string appart;
+    int x = 0;
+    int y = 0;
 
     int count = 0;
     file >> count;
-    //count = std::stoi(infile);
 
-    std::string* bd = creat_arr(count);
+
+    Adress* bd = new Adress[count];
+
 
     for (int i = 0; i < count; ++i) {
         file >> town >> street >> building >> appart;
-        Adress adress(town, street, building, appart);
-        bd[i] = adress.create_fulladress();
+        x = bd[i].converting(building);
+        y = bd[i].converting(appart);
+        bd[i].set_adress(town, street, x, y);
     }
 
+    for (int j = 0; j < (count - 1); ++j) {
+        for (int i = 0; i < (count - 1); i++) {
+            std::string t1 = bd[i].get_town();
+            std::string t2 = bd[i + 1].get_town();
+            if (t1 > t2) {
+                bd[i].set_convert(bd[i], bd[i + 1]);
+            }
+        }
+    }
+
+
+    std::ofstream offfile("out.txt"); 
+    if (offfile.is_open()) {
+        std::cout << "Файл out.txt открыт." << std::endl;
+    }
+    else {
+        std::cout << "Ошибка открытия файла" << std::endl;
+    }
+
+    offfile << count << "\n";
+    for (int i = 0; i < count; i++) {
+        offfile << bd[i].get_town() << ", " << bd[i].get_street() << ", " << bd[i].get_building() << ", "
+            << bd[i].get_appart() << "\n";
+    }
+
+    delete[]bd;
     file.close();
-    sort(bd, count);
-    print_arr(bd, count);
-    delete_arr(bd, count);
+    offfile.close();
+
+    return 0;
 
 
 }
